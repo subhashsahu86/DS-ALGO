49f31c587e21e205662e2175e2ab40c876d9bd2d
subhash <subhashsahu.dev@gmail.com>
2026-06-24 23:40:48 +0530

    added Prob217_ContainsDuplicate

--- DS_ALGO_PREP/src/main/java/com/subh/leetcode/array/Prob217_ContainsDuplicate.java
+++ DS_ALGO_PREP/src/main/java/com/subh/leetcode/array/Prob217_ContainsDuplicate.java
@@ -1,8 +1,46 @@
 package com.subh.leetcode.array;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Prob217_ContainsDuplicate {
 
     public static void main(String[] args) {
 
+        int [] nums = {1,2,3,4};
+
+        boolean res = findDuplicates(nums);
+
+        System.out.println(res);
+
+
+
     }
+
+    public static boolean findDuplicates(int [] nums){
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //method 2
+
+        public boolean containsDuplicate(int[] nums) {
+            HashSet<Integer> set = new HashSet<>();
+            for (int num:nums) {
+                if (set.contains(num)) {
+                    return true;
+                }
+                set.add(num);
+            }
+            return false;
+        }
+
 }
